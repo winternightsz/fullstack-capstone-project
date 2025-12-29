32ff6875b39e7db95ef7a1aea8c5c1c04e81f44f
winternightsz <izabelaandreazza@gmail.com>
2025-12-29 02:14:42 -0500

    Adding Update API to Frontend

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,9 +4,11 @@ import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
+import Navbar from './components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+
 function App() {
   const navigate = useNavigate();
   return (
@@ -18,6 +20,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
   );
