9a917badd58cfac9eec855777c2f2bf86d0f19a3
winternightsz <izabelaandreazza@gmail.com>
2025-12-29 02:05:46 -0500

    Login API for Frontend

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,13 +1,62 @@
 import React, { useState,useEffect } from 'react';
-
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 
 function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState(''); // state for incorrect password.
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
+    //local variable for `navigate`,`bearerToken`   and `setIsLoggedIn`.
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+
+    //If the bearerToken has a value (user already logged in), navigate to MainPage
+    useEffect(() => {
+    if (sessionStorage.getItem('auth-token')) {
+        navigate('/app')
+    }
+    }, [navigate])
+
+const handleLogin = async () => {
+    e.preventDefault();
+    const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+        },
+        //Set body to send user details
+        body: JSON.stringify({
+            email: email,
+            password: password,
+        })
+    });
+
+    // Access data coming from fetch API
+    const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+            // Set user details
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', json.userName);
+            sessionStorage.setItem('email', json.userEmail);
+            //Set the user's state to log in using the `useAppContext`.    
+            setIsLoggedIn(true);
+            navigate('/app');
+        } else {
+            //Clear input and set an error message if the password is incorrect
+            document.getElementById("email").value="";
+            document.getElementById("password").value="";
+            setIncorrect("Wrong password. Try again.");
+            setTimeout(() => {
+                setIncorrect("");
+            }, 2000);
+        }
 }
 
 
@@ -38,8 +87,9 @@ function LoginPage() {
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                             />
+                            <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
                         </div>
-                        {/* Include appropriate error message if login is incorrect*/}
+                        
                         <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
                         <p className="mt-4 text-center">
                             New here? <a href="/app/register" className="text-primary">Register Here</a>
